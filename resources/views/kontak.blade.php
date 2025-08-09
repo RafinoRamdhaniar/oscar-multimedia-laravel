f23431ab38d154731e2c8105fb308274ab181a9b
Fino <finoraf01@gmail.com>
2025-08-09 10:13:03 +0700

    icon di kontak

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -56,6 +56,7 @@
             text-decoration: underline;
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     {{-- Navbar --}}
