2ed110e3e22ee1b3341d6efd1add0cad63cc5481
Fino <finoraf01@gmail.com>
2025-09-20 09:08:07 +0700

    .

--- resources/views/layouts/partials/highlight-product.blade.php
+++ resources/views/layouts/partials/highlight-product.blade.php
@@ -67,7 +67,11 @@
     .carousel-caption-overlay {
         position: absolute;
         top: 0; left: 0; right: 0; bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
+        /* background-color: rgba(0, 0, 0, 0.5); */
+        background: linear-gradient(
+        rgba(0,0,0,0.15), 
+        rgba(0,0,0,0.15)
+    );
     }
 </style>
 
